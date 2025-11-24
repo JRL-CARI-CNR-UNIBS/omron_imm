76d9b2783b3adbf655f3a5b04d39b9dc2e67c27b
RobertoFausti <you@example.com>
2025-11-24 13:59:16 +0100

    Local changes for moveit standalone launch and config

--- omron_imm_bringup/launch/tm_moveit_standalone.launch.py
+++ omron_imm_bringup/launch/tm_moveit_standalone.launch.py
@@ -12,6 +12,11 @@ def generate_launch_description():
     default_value=PathJoinSubstitution([FindPackageShare('omron_imm_bringup'), 'config', 'tm_moveit_standalone.yaml']),
     description='Full path to the config file to load'
   )
+  namespace = DeclareLaunchArgument(
+    name='namespace',
+    default_value='omron',
+    description='Namespace of each node'
+  )
 
   tm_moveit_standalone = Node(
     package='stiima_tm12_hw',
@@ -19,6 +24,6 @@ def generate_launch_description():
     name='tm_moveit_standalone_node',
     output='screen',
     parameters=[LaunchConfiguration('config_path')],
-
+    namespace=LaunchConfiguration('namespace')
   )
-  return LaunchDescription([config_path, tm_moveit_standalone])
+  return LaunchDescription([config_path, namespace, tm_moveit_standalone])
